df52eaf5474731268a61401344cacb6a279bb0b4
Suwamik Paul <Suwamik Paul>
2017-01-21 18:10:29 -0500

    selecting characters is complete

--- final.c
+++ final.c
@@ -16,7 +16,7 @@
 #include "client.c"
 
 struct character{
-  int name;
+  char* name;
   int hp;
   int ap;
   int atk;
@@ -24,7 +24,6 @@ struct character{
   int matk;
   int mdef;
   int initv;
-  int friend;
 };
 
 struct character man1;
@@ -53,19 +52,17 @@ int randint() {
   int fd = open( "/dev/random", O_RDONLY ); //open file
   int *buff = malloc(sizeof(int)); //initialize buff by allocating mem
   read( fd, buff, sizeof(int) ); //write first 4 bytes to buff
-
   close(fd); //close
-
   return *buff; //dereference randomly generated number
 }
 
 void addToStruct( struct character *dude, char *stuff ){
   int w = 0;
   char * x;
-  while ( w < 9 ){
+  while ( w < 8 ){
     x = strsep(&stuff, ",");
     if (w == 0)
-      dude -> name = atoi(x);
+      dude -> name = x;
     else if (w == 1)
       dude -> hp = atoi(x);
     else if (w == 2)
@@ -78,10 +75,8 @@ void addToStruct( struct character *dude, char *stuff ){
       dude -> matk = atoi(x);
     else if (w == 6)
       dude -> mdef = atoi(x);
-    else if (w == 7)
+    else 
       dude -> initv = atoi(x);
-    else
-      dude -> friend = atoi(x);
     w++;
   }
 }
@@ -95,13 +90,15 @@ void copystruct(struct character *s1, struct character *s2){
   s1 -> matk = s2 ->matk;
   s1 -> mdef = s2 ->mdef;
   s1 -> initv = s2 -> initv;
-  s1 -> friend = s2 -> friend;
 }
 
-void initialize( ) {  
+void initialize(char * c1, char * c2, char *c3) {  
   char ** gogojuice = (char **)malloc(sizeof(char*));
   char ** daddy = (char **)malloc(sizeof(char*));
-  
+
+  printf("c1: %s\n",c1);
+  printf("c2: %s\n",c2);
+  printf("c3: %s\n",c3);
   int fd = open( "Players.txt", O_RDONLY );
   char buff[1024];
   read( fd, buff, sizeof(buff) );
@@ -119,23 +116,27 @@ void initialize( ) {
   }
   printf("test1\n");
   int x = 0;
+  int z = 0;
+
+  //printf("%s\n",strstr(daddy[1],"Gordon"));
+  
   for ( x; x < i - 1; x++ ){
-    printf("%d\n", daddy[x]);
-    if(x == 0){
+    if (strstr(daddy[x],c1) != NULL){
+      printf("%s\n", daddy[x]);
       addToStruct( &man1, daddy[x] );
-      party[x] = man1;
+      z++;
     }
-    else if(x == 1){
-      //printf("%s\n",daddy[x]);	    
-      //printf("affirmative\n");
+    else if (strstr(daddy[x],c2) != NULL){
+      printf("%s\n", daddy[x]);
       addToStruct( &man2, daddy[x] );
-      party[x] = man2;
+      party[z] = man2;
+      z++;
     }
-    else if(x == 2){
-      //printf("%s\n",daddy[x]);	    
-      //printf("affirmative\n");
-      addToStruct( &man3, daddy[x] );
-      party[x] = man3;
+    else if (strstr(daddy[x],c3) != NULL){
+      printf("%s\n", daddy[x]);
+      addToStruct( &man2, daddy[x] );
+      party[z] = man3;
+      z++;
     }
     //gogojuice[x] = strsep(&daddy[x], ",");
     //printf("%s\n", gogojuice[x]);
@@ -171,163 +172,131 @@ int main(){
     printf("Please enter IP address of server you would like to join\n");
     //fgets(k,sizeof(k),stdin);
     //args[1] = k;
-    sendserv("custom message for server\n"); // allows you to send custom info from client to the server (string only)
+    //sendserv("custom message for server\n"); // allows you to send custom info from client to the server (string only)
     set1(1);
     clien(1,args);
     //set1(0);
   }
-	  
-  
-  //char * t = "Snowden,5,5,5,5,5,5,5,Edward";
-  //addToStruct(&test,t); didnt work for some reason
-  test.name = 7;
-  test.hp = 5;
-  test.ap = 5;
-  test.atk = 5;
-  test.def = 5;
-  test.matk = 5;
-  test.mdef = 5;
-  test.initv = 5;
-  test.friend = 4;
-  //printf("%s\n\n", test.friend);
-  
-  initialize();
-  printf("test2\n");
-  printf("[1] name: %d; friend: %d\n", man1.name, man1.friend);
-  printf("[2] name: %d; friend: %d\n", man2.name, man2.friend);
-  printf("\n");
-  //printf("%s\n", man1.friend);
-  //printf("%s\n", man2.friend);
-  int enemy = 3;
-  int enemyd = 0;
-  int player = 3;
-  int playerd = 0;
-  // make array with all classes
-  //then array for ones in use
-  struct character option[12];
-  copystruct(&option[0], &man1);
-  copystruct(&option[1], &man2);
-  copystruct(&option[2], &man3);
-  printf("first: %d\n", option[0].name);
-  printf("first: %d\n", option[1].name);
-  printf("first: %d\n", option[2].name);
-  struct character using[3];
-  int  pick[3];
+
+  char t[] = "Edward,5,5,5,5,5,5,5";
+  addToStruct(&test,t);
+
   int chosen = 0;
+  
   while(1){
     if(!chosen){
-      printf("Welcome\n");
-      printf("Choose 3 characters with the corresponding number keys seperated by spaces\n");
-      printf("(0)Gordon [Bad]\n");
-      printf("(1)Percy [Decent stats]\n");
-      printf("(2)Thomas [All-around high stats]\n");
+      char c1[256];
+      char c2[256];
+      char c3[256];
       char class[266];
-      fgets(class,sizeof(class),stdin); // this first fgets is getting something else strange, need to do twice
-      char class1[266];
-      fgets(class1,sizeof(class1),stdin);
-      printf("class1: %s\n", class1);
-      //copystruct(&using[0], &option[atoi(class1[0])]); /////////////////NEED TO MAKE IT ADD TO USING
-      //copystruct(&using[1], &option[atoi(class1[2])]);
-      //copystruct(&using[2], &option[atoi(class1[4])]);
-      pick[0] = class1[0];
-      pick[1] = class1[2];
-      pick[2] = class1[4];
-      printf("working: %d\n", pick[0]);
-      printf("working: %d\n", pick[1]);
-      printf("working: %d\n", pick[2]);
-      //if (isServer == 1){
-      //sendclient("0,1,2"); // by putting this here can send custom info from server to client (string only)
-      //set(1);
-      //serve();
-      //}
-      //char *args[2];
-      //args[0]= "client";
-  
-      //     if (isServer == 0){
-      //sendserv("0,1,2"); // allows you to send custom info from client to the server (string only)
-      //set1(1);
-      //clien(1,args);
-    //set1(0);
-      //}
-    ///NETWORKING DOES NOT WORK SERVER IN USE ERROR OR SOMETHING 
-      chosen = 1;
+      char class2[266];
+      printf("Welcome\n");
 
-
-    }
-    else{     
-    int ai = randint() % 3;
-    if(!enemy){
-      printf("you win\n");
-      return 0;
-    }
-    if(!player){
-      printf("you lose\n");
-      return 0;
-    }
-    char a[256];
-    printf("Enemy health is: %d\n", enemy);
-    //printf("Your health is: %d\n", player); 
-    printf("Enter a to attack, enter s to cast spell, enter d to defend\n");
-    fgets(a,sizeof(a),stdin);
-    playerd = 0;
-    if(a[0] == 'a'){
-      printf("Which enemy?"); //gonna have an array of enemies to select who to attack
+      int k = 0;
+      while (k < 3){
+	if (k == 0)
+	  printf("\nChoose Your first character. Type the name\n");
+	if (k == 1)
+	  printf("\nChoose your second character. Type the name\n");
+	if (k == 2)
+	  printf("\nChoose Your third character. Type the name\n");
+	
+	//where we would need to list all the possible people, preety easy
+	printf("Gordon [Bad]\n");
+	printf("Percy [Decent stats]\n");
+	printf("Thomas [All-around high stats]\n");
+	if (k == 0)
+	  fgets(class2,sizeof(class2),stdin); // this first fgets is getting something else strange, need to do twice	
+	fgets(class,sizeof(class),stdin);
+	class[strcspn(class, "\n")] = 0;
+	if (k == 0)
+	  strcpy(c1,class);
+	if (k == 1)
+	  strcpy(c2,class);
+	if (k == 2)
+	  strcpy(c3,class);
+	k++;
+      }
+      printf("we made it here\n");
+      initialize(c1,c2,c3);
       
-      if(enemyd == 1){
-	printf("enemy defends attack\n");
-	//at this point only gordon is attack for test purposes
-	// also the 10 is relative at this point
-	damage(man1.atk, test.def + 10, test.hp);
+      chosen = 1;
+ 
       }
-      else{
-	damage(man1.atk, test.def, test.hp);
+  else{
+      /*int ai = randint() % 3;
+      if(!enemy){
+	printf("you win\n");
+	return 0;
       }
-    }
-    // we cant do magic attacks until we get the different spells we are going to use
-    if(a[0] == 's'){
-      if(enemyd == 1){
-	printf("enemy defends spell\n");
+      if(!player){
+	printf("you lose\n");
+	return 0;
       }
-      else{
-	enemy --;
+      char a[256];
+      printf("Enemy health is: %d\n", enemy);
+      //printf("Your health is: %d\n", player); 
+      printf("Enter a to attack, enter s to cast spell, enter d to defend\n");
+      fgets(a,sizeof(a),stdin);
+      playerd = 0;
+      if(a[0] == 'a'){
+	printf("Which enemy?\n"); //gonna have an array of enemies to select who to attack
+      
+	if(enemyd == 1){
+	  printf("enemy defends attack\n");
+	  //at this point only gordon is attack for test purposes
+	  // also the 10 is relative at this point
+	  damage(man1.atk, test.def + 10, test.hp);
+	}
+	else{
+	  damage(man1.atk, test.def, test.hp);
+	}
       }
-    }
-    if(a[0] == 'd'){
-      playerd ++; // implement further
-    }
+      // we cant do magic attacks until we get the different spells we are going to use
+      if(a[0] == 's'){
+	if(enemyd == 1){
+	  printf("enemy defends spell\n");
+	}
+	else{
+	  enemy --;
+	}
+      }
+      if(a[0] == 'd'){
+	playerd ++; // implement further
+      }
 
-    // glitch sometimes no action is taken
-    enemyd = 0;
-    if(ai == 0){
-      if(playerd == 1){
-	//should randomly select an enemy
-	printf("You defended enemy attack\n");
-	damage(test.atk, man1.def + 10, man1.hp);	
+      // glitch sometimes no action is taken
+      enemyd = 0;
+      if(ai == 0){
+	if(playerd == 1){
+	  //should randomly select an enemy
+	  printf("You defended enemy attack\n");
+	  damage(test.atk, man1.def + 10, man1.hp);	
+	}
+	else{
+	  printf("Enemy attacks\n");
+	  damage(test.atk, man2.def + 10, man1.hp);
+	}
       }
-      else{
-	printf("Enemy attacks\n");
-	damage(test.atk, man2.def + 10, man1.hp);
+      if(ai == 1){
+	if(playerd == 1){
+	  printf("You defended enemy spell\n");
+	}
+	else{
+	  printf("Enemy casts spell\n");
+	  player --;
+	}
       }
-    }
-    if(ai == 1){
-      if(playerd == 1){
-	printf("You defended enemy spell\n");
+      if(ai == 2){
+	printf("Enemy defends\n");
+	enemyd ++;// implement further 
       }
-      else{
-	printf("Enemy casts spell\n");
-	player --;
-      }
-    }
-    if(ai == 2){
-      printf("Enemy defends\n");
-      enemyd ++;// implement further 
-    }
     
     
-    printf("------------------\n");
+      printf("------------------\n");
   
     
-    }
+      }*/
   }
   return 0;
 }
